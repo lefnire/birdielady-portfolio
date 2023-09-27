4c0484de4ec8262d7e6add9c225bd5999bfa5ae7
Martha Wilding <wilding34@gmail.com>
2023-09-26 20:54:11 -0700

    Add navbar, footer, and fonts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Martha Musselman's Portfolio" },
+    { name: "Professional portfolio", content: "" },
   ];
 };
 
@@ -13,10 +13,11 @@ export default function Index() {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Martha Musselman</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="text-5xl font-display font-medium">Martha Musselman</h1>
+      <p className="py-6 font-body">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Download CV</button>
     </div>
   </div>
 </div>
 }
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,20 @@
 import type { Config } from 'tailwindcss'
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'display': ['League Spartan'],
+      'body': ['Poppins']
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [require("daisyui")],
 } satisfies Config
-
