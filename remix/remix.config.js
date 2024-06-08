14d97e328c87960f7fd406e09d8e2e9a49fa339b
Tyler Renelle <tylerrenelle@gmail.com>
2024-06-08 13:00:25 -0700

    wire sst to remix + domain. Remove unused packages/*

---
+++ remix/remix.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm",
+};
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,30 +1,16 @@
-import { StackContext, Api, EventBus } from "sst/constructs";
+import { StackContext, RemixSite } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
+  const site = new RemixSite(stack, "Site", {
+    path: "remix/",
+    customDomain: {
+      domainName: "marthawilding.com",
+      domainAlias: "www.marthawilding.com",
     },
   });
 
-  const api = new Api(stack, "api", {
-    defaults: {
-      function: {
-        bind: [bus],
-      },
-    },
-    routes: {
-      "GET /": "packages/functions/src/lambda.handler",
-      "GET /todo": "packages/functions/src/todo.list",
-      "POST /todo": "packages/functions/src/todo.create",
-    },
-  });
-
-  bus.subscribe("todo.created", {
-    handler: "packages/functions/src/events/todo-created.handler",
-  });
-
+  // Add the site's URL to stack output
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    URL: site.url || "localhost",
   });
 }
